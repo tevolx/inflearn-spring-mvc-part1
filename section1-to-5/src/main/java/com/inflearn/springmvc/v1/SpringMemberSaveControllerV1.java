73efad1971073e762961daac6035a73d65ae744e
hyuntae-park <hyuntae.park1030@navercorp.com>
2023-07-23 21:55:30 +0900

    feat: 디렉토리 구조 변경

---
+++ section1-to-5/src/main/java/com/inflearn/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,22 @@
+//package com.inflearn.springmvc.v1;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+///**
+// * SpringMemberSaveControllerV1
+// *
+// * @author HYUN TAE PARK
+// * @version 1.0.0
+// * @since 2023. 05. 29.
+// */
+//
+//@Controller
+//public class SpringMemberSaveControllerV1 {
+//
+//	@RequestMapping("/springmvc/v1/members/save")
+//	public ModelAndView process() {
+//
+//	}
+//}
